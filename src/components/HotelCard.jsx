3fbc334965b56ebfb73af043dde6028833686a0c
unknown <khanhnguyen2709kn@gmail.com>
2026-05-11 13:36:24 +0700

    test login UI

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,5 +1,19 @@
-const HotelCard = () => {
-  return <div></div>;
+const HotelCard = ({ room, index }) => {
+  return (
+    <Link
+      to={"/rooms/" + room.id}
+      onClick={() => window.scrollTo(0, 0)}
+      key={room.id}
+    >
+      <img src={room.image[0]} alt={""} />
+      <p>Best Seller</p>
+      <div>
+        <div className="flex items-center justify-between">
+          <p className="font-playfair text-xl font-medium text-gray-800"></p>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default HotelCard;
